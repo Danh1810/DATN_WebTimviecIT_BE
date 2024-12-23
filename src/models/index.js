ba9b21e01bf1e9a9f8fa06830bb5117cee899351
Danh1810 <99806310+Danh1810@users.noreply.github.com>
2024-12-23 14:52:01 +0700

    ok

--- src/models/index.js
+++ src/models/index.js
@@ -10,24 +10,29 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
+
+// Check if `use_env_variable` is defined in the config
 if (config.use_env_variable) {
+  // Use the environment variable to initialize Sequelize
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  // Fallback to directly using the configuration details
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    port: config.port, // Include the port if defined
+    logging: false, // Optional: disable SQL query logging
+  });
 }
 
+// Dynamically import all models in the current directory
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
+      file.indexOf(".") !== 0 && // Exclude hidden files
+      file !== basename && // Exclude this file
+      file.slice(-3) === ".js" && // Include only .js files
+      file.indexOf(".test.js") === -1 // Exclude test files
     );
   })
   .forEach((file) => {
@@ -38,13 +43,14 @@ fs.readdirSync(__dirname)
     db[model.name] = model;
   });
 
+// Run model associations, if any
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize; // Attach the Sequelize instance
+db.Sequelize = Sequelize; // Attach the Sequelize library
 
 module.exports = db;
